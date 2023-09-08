0ec96dd1341f0632e8c773b7c270aec957314e1f
Luke Boggs <lukeboggs7@gmail.com>
2023-09-07 22:32:18 -0500

    Fetch logged in users spotify id

--- pages/api/create-playlist.js
+++ pages/api/create-playlist.js
@@ -7,26 +7,18 @@ const SPOTIFY_BASE_URL = "https://api.spotify.com/v1";
 
 export default async function (req, res) {
 
-  const theme = req.body.theme;
-
-  const songs = req.body.songs;
-
   try {
-    // const authData = {
-    //   grant_type: "client_credentials",
-    //   client_id: process.env.SPOTIFY_CLIENT_ID,
-    //   client_secret: process.env.SPOTIFY_CLIENT_SECRET
-    // };
-
-    // const tokenResponse = await axios.post(
-    //   "https://accounts.spotify.com/api/token",
-    //   authData,
-    //   { headers: { 'content-type': 'application/x-www-form-urlencoded' } }
-    // );
-
-    // console.log(tokenResponse);
-
     const session = await getServerSession(req, res, authOptions);
+
+    const profileResponse = await axios.get(
+      `${SPOTIFY_BASE_URL}/me`,
+      { headers: { 'Authorization': `Bearer ${session.accessToken}` } }
+    );
+
+    console.log("------------profileResponse----------------");
+    console.log(profileResponse);
+
+    const profileId = profileResponse.data.id;
 
     const playlistRequestData = {
       "name": req.body.theme,
@@ -34,7 +26,7 @@ export default async function (req, res) {
     };
 
     const createPlaylistResponse = await axios.post(
-      `${SPOTIFY_BASE_URL}/users/${process.env.SPOTIFY_DEFAULT_USER_ID}/playlists`,
+      `${SPOTIFY_BASE_URL}/users/${profileId}/playlists`,
       playlistRequestData,
       { headers: { 'Authorization': `Bearer ${session.accessToken}` } }
     );
@@ -53,7 +45,6 @@ export default async function (req, res) {
 
     res.status(200).json({ id: createPlaylistResponse.data.id });
   } catch (error) {
-    // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
@@ -74,11 +65,6 @@ async function searchSpotify(track, artist, accessToken) {
     `${SPOTIFY_BASE_URL}/search?q=${track} ${artist}&type=track&limit=1`,
     { headers: { 'Authorization': `Bearer ${accessToken}` } }
   );
-
-  console.log("-------------search response-----------------");
-  console.log(resp);
-  console.log(resp.data.tracks.items);
-  console.log(resp.data.tracks.items[0].id);
 
   return resp.data.tracks.items[0]?.id;
 }
